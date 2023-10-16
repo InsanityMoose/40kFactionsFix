3f4b4c07597856b7342a6c7884dcef81c0673aa4
InsanityMoose <docoma@gmail.com>
2023-10-16 21:30:17 +0200

    Patched Infocard
    Patched InfoCard to actually show the factions tab as well

---
+++ Source/Patch_InfoCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+
+namespace _40kFactionsFix
+{
+    [HarmonyPatch(typeof(Dialog_InfoCard), nameof(Dialog_InfoCard.DoWindowContents))]
+    public static class Patch_DoWindowContents
+    {
+        public static void Prefix(Rect inRect)
+        {
+            foreach (Faction faction in Find.FactionManager.AllFactionsVisible)
+            {
+                if (!faction.IsPlayer && !faction.def.permanentEnemy && !faction.temporary)
+                {
+                    foreach (RoyalTitlePermitDef allDef in DefDatabase<RoyalTitlePermitDef>.AllDefs)
+                    {
+                        if (allDef.faction == faction.def)
+                        {
+                            if (PermitsCardUtility.selectedFaction == null) PermitsCardUtility.selectedFaction = faction;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
